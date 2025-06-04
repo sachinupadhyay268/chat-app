f9b2dc33566fe1cfceda25ca94cd4f898ee402ba
Sachin Upadhyay <sachinupadhyay@Sachins-MacBook-Air.local>
2025-06-04 12:51:00 +0530

    sidebar 55.47

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -44,7 +44,12 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
 
       <div className="flex flex-col">
         {userDummyData.map((user, index) => (
-          <div>
+          <div onClick={() => {setSelectedUser(user)}}
+          key={index}
+            className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${
+              selectedUser?._id === user.id && "bg-[#282142]/50"
+            }`}
+          >
             <img
               src={user?.profilePic || assets.avatar_icon}
               alt="profilepic"
@@ -52,10 +57,18 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
             />
             <div className="flex flex-col leading-5">
               <p>{user.fullName}</p>
-              {index > 3 ? <span className="text-green-400 text-xs ">online</span> : <span className="text-neutral-400 text-xs">offline</span>}
+              {index < 3 ? (
+                <span className="text-green-400 text-xs ">online</span>
+              ) : (
+                <span className="text-neutral-400 text-xs">offline</span>
+              )}
             </div>
 
-            {index > 2 && <p className="absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50">{index}</p>}
+            {index > 2 && (
+              <p className="absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50">
+                {index}
+              </p>
+            )}
           </div>
         ))}
       </div>
